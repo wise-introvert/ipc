fcac0c560323a3c8bccd3dbf26949c571f22fc78
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-09-29 12:18:58 -0400

    feat: week 1

---
+++ w1/w1p1.c
@@ -0,0 +1,14 @@
+/*
+ * AUTHOR      Fardeen Panjwani
+ * EMAIL       fspanjwani@myseneca.ca
+ * SENECA ID   119588218
+ * DESCRIPTION Print message to the screen
+ */
+
+#include<stdio.h>
+
+int main(void) {
+  printf("*** Welcome to Seneca's C Programming Course ***\n");
+
+  return 0;
+}
---
+++ w1/w1p2.c
@@ -0,0 +1,18 @@
+/*
+ * AUTHOR      Fardeen Sohil Panjwani
+ * EMAIL       fspanjwani@myseneca.ca
+ * SENECA ID   119588218
+ * DESCRIPTION Print a beautifully-formatted message to the screen
+ */
+
+#include<stdio.h>
+
+int main(void) {
+  printf("+-------------------------------+\n");
+  printf("| Introduction to C Programming |\n");
+  printf("+-------------------------------+\n");
+  printf("Program written by:\n");
+  printf("Fardeen Sohil Panjwani\n");
+
+  return 0;
+}
